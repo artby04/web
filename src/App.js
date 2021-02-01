d01f5d040eb4a5516934945f1b5a8e9685fdc3f9
artby04 <52202830+artby04@users.noreply.github.com>
2021-02-01 21:17:04 +0300

    Test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<header className="header">
+				<div className="header__content">
+					<div className="header__logo">
+						<img src={logo} alt="Logo" />
+					</div>
+				</div>
+			</header>
+			<nav className="nav">
+				<div>
+					<a href="">Profile</a>
+				</div>
+				<div>
+					<a href="">Messages</a>
+				</div>
+				<div>
+					<a href="">News</a>
+				</div>
+				<div>
+					<a href="">Music</a>
+				</div>
+				<div>
+					<a href="">Settings</a>
+				</div>
+			</nav>
+			<div className="content">
+				<img src="https://miro.medium.com/max/1050/1*Fxa6oJFh2lRD7EnAcZIcQg.png" alt="" />
+			</div>
+		</div>
+	);
 }
 
 export default App;
