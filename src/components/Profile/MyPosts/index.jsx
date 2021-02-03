fe965a616b0e8f42d24122b67a08b8925c7be324
artby04 <52202830+artby04@users.noreply.github.com>
2021-02-03 22:38:42 +0300

    Fixed

--- src/components/Profile/MyPosts/index.jsx
+++ src/components/Profile/MyPosts/index.jsx
@@ -6,7 +6,7 @@ const MyPosts = () => {
 		{ id: 1, message: "Hey how are you?", likeCount: 12 },
 		{ id: 2, message: "Yo man see", likeCount: 42 },
 		{ id: 3, message: "YEs asd it mor", likeCount: 62 },
-		{ id: 4, message: "Yo yeah", likeCount: 43 }
+		{ id: 4, message: "Yo yeah", likeCount: 98 },
 	];
 
 	let postsElemenst = postsData.map((p) => <Post message={p.message} likeCount={p.likeCount} />);
