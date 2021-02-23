63b12a804941975baba5842fd549461c4ca9e9e1
artby04 <52202830+artby04@users.noreply.github.com>
2021-02-24 00:17:34 +0300

    43

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter, Route } from "react-router-dom";
 import News from "./components/News";
 import Settings from "./components/Settings";
 import Music from "./components/Music";
-import store from "./redux/state";
 
 function App(props) {
 	return (
